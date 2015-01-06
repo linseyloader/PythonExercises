7f951fdf83fbb4594b87b5027b448ad0d426c337
Linsey Loader <linseyloader@gmail.com>
2015-01-06 11:26:46 -0800

    Updated project 1.

--- Exercise1/exercise_1.py
+++ Exercise1/exercise_1.py
@@ -1,5 +1,15 @@
-nu = input("Please enter the first integer: ")
-nm = input("Please enter the second integer: ")
+nu = raw_input("Please enter the first integer: ")
+while not nu.isdigit() or nu == "0":
+    if nu == "0":
+        nu = raw_input("Cannot determine using 0. Try another number: ")
+    else:
+        nu = raw_input("That's not a number. Try again: ")
+nm = raw_input("Please enter the second integer: ")
+while not nm.isdigit() or nm == "0":
+    if nm == "0":
+        nm = raw_input("Cannot determine using 0. Try another number: ")
+    else:
+        nm = raw_input("That's not a number. Try again: ")
 nu_value = int(nu)
 nm_value = int(nm)
 print "The sum of", (nu_value), "and", (nm_value), "is:", (nu_value + nm_value)
